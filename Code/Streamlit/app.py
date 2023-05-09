b4cbb774ee8f81edad6accdd3431d38f017a8c77
Victor <victor>
2023-05-08 20:01:42 -0500

    changes to requirements5

--- Code/Streamlit/app.py
+++ Code/Streamlit/app.py
@@ -8,7 +8,7 @@ import nltk
 #nltk.download()
 nltk.download('wordnet')
 nltk.download('stopwords')
-#nltk.download('punkt')
+nltk.download('punkt')
 #nltk.download('popular')
 from nltk.corpus import stopwords
 from collections import Counter
